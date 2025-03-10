0190c3ea48f651f1561c06742f43c7f520a147f6
hs-2171098-hyunjinchoi <chju0113@naver.com>
2025-03-11 00:26:57 +0900

    feat: 전역 상태 관리를 위한 컨텍스트 추가
    - 토큰과 사용자 정보 각각 관리

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,51 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
+import { mockRepoInfo } from "../assets/mockData";
+
+interface UserInfo {
+  login: string;
+  avatar_url: string;
+}
+
+interface UserContextType {
+  userInfo: UserInfo | null;
+  setUserInfo: (user: UserInfo) => void;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUser must be used within a UserProvider");
+  }
+  return context;
+};
+
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+
+  useEffect(() => {
+    if (import.meta.env.MODE === "development") {
+      console.warn("Vite dev env: Using Mock User Data");
+      setUserInfo(mockRepoInfo.userInfo);
+    } else {
+      chrome.storage.local.get(["GitHub_User_Info"], (data) => {
+        if (data.GitHub_User_Info) {
+          setUserInfo(data.GitHub_User_Info);
+        }
+      });
+    }
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
---
+++ src/pages/Popup.tsx
@@ -0,0 +1,45 @@
+import "../style.css";
+import LoginScreen from "./login/LoginPage";
+import SearchScreen from "./search/SearchPage";
+import { TokenProvider, useToken } from "../context/TokenContext";
+import { UserProvider } from "../context/UserContext";
+import { mockToken } from "../assets/mockData";
+import { useEffect } from "react";
+
+const PopupContent = () => {
+  const { token, setToken } = useToken();
+
+  useEffect(() => {
+    if (import.meta.env.MODE === "development") {
+      console.warn("Vite Dev Env: Using Mock Token");
+      setToken(mockToken);
+    } else {
+      chrome.storage.local.get(["GitHub_Access_Token"], (data) => {
+        if (data.GitHub_Access_Token) {
+          setToken(data.GitHub_Access_Token);
+        }
+      });
+    }
+  }, []);
+
+  return (
+    <div className="popup-container">
+      {/* <h1 className="app-title">Commit Finder</h1>
+      <h2 className="subtitle">GitHub Commit & Comment Search</h2> */}
+
+      {!token ? <LoginScreen /> : <SearchScreen />}
+    </div>
+  );
+};
+
+const Popup = () => {
+  return (
+    <TokenProvider>
+      <UserProvider>
+        <PopupContent />
+      </UserProvider>
+    </TokenProvider>
+  );
+};
+
+export default Popup;
